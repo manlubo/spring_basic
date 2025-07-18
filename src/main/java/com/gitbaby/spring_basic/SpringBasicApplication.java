fd7544aa6691d7045a762feaf4b9f1bcb3710537
manlubo <manlubo11@gmail.com>
2025-07-18 17:40:50 +0900

    AOP, 프로시저

--- src/main/java/com/gitbaby/spring_basic/SpringBasicApplication.java
+++ src/main/java/com/gitbaby/spring_basic/SpringBasicApplication.java
@@ -3,9 +3,12 @@ package com.gitbaby.spring_basic;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Slf4j
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class SpringBasicApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/gitbaby/spring_basic/domain/Member.java
+++ src/main/java/com/gitbaby/spring_basic/domain/Member.java
@@ -1,14 +1,27 @@
 package com.gitbaby.spring_basic.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Data
-@Component
+//@Component
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Member {
-  @Value("Winter")
+//  @Value("Winter")
   private String name;
-  @Value("32")
+//  @Value("32")
   private int age;
+  private String id;
+
+  public Member(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
 }
